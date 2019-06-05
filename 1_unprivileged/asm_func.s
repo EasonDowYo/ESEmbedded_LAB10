6a7bb0c9e197d2fd2611340f0360ad75963c3254
EasonDowYo <king0906sk@gmail.com>
2019-06-05 09:17:31 +0800

    modified

--- 1_unprivileged/asm_func.s
+++ 1_unprivileged/asm_func.s
@@ -2,24 +2,34 @@
 
 .global	read_sp
 read_sp:
-	??????
-
+	mov r0,sp
+	bx lr
 .global	read_msp
 read_msp:
-	??????
-
+	mrs r0,msp
+	bx lr
 .global	read_psp
 read_psp:
-	??????
-
+	mrs r0,psp
+	bx lr
 .global	read_ctrl
 read_ctrl:
-	??????
+	mrs r0,control
+    bx lr
 
 .global	start_user
 start_user:
-	??????
+	mov lr,r0
+    msr psp,r1
+	mov r0,#0b11
+    msr control,r0
+    isb
+    bx lr
+    
+
 
 .global	sw_priv
 sw_priv:
-	??????
+	mov r0,#0b00
+	msr control,r0
+    bx lr
